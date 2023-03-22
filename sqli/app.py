f746e7763d416afdf34a372b5c67f3ab8904704c
Sham-Report <63191240+Sham-Report@users.noreply.github.com>
2023-03-22 12:32:48 +0530

    Update app.py

--- sqli/app.py
+++ sqli/app.py
@@ -43,17 +43,5 @@ AWS_ACCESS_KEY = 'AKIAQWESQRFOQ4KOQAPK'
 AWS_SECRET_KEY = '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'
 AWS_CONSOLE_PASSWORD = '$ecureAdmin1000'
 AWS_SECRET_KEY = 'SDyoOe0/so/muXBufjrQs9wDcFCsE10000/2DWbl'
+AWS_SECRET_KEY = 'SDyoOe0/so/muXBufjrQs9wDcFCsE20000/2DWbl'
 
-
-Subdomain enumeration: discovering all subdomains related to the domain of an organization.
-DNS records analysis: inspecting DNS records to gather information about the infrastructure of an organization.
-Web server fingerprinting: identifying the technology stack of a web server by analyzing the HTTP response headers.
-IP address mapping: discovering how many subdomains are mapped to the same IP address.
-Open port scanning: identifying open ports on servers to identify potential attack vectors.
-Screenshots of web applications: taking screenshots of web applications to visually inspect them for potential security issues.
-Service enumeration: identifying which services are running on open ports and their versions.
-Operating system detection: identifying the operating system running on a target system.
-Subdomain expiry date analysis: discovering the expiry date of subdomains to identify potential domains that could be claimed by malicious actors.
-SSL certificate analysis: determining which Certificate Authority (CA) has issued SSL certificates for a target domain and when they will expire.
-Reputation analysis: analyzing an organization's reputation in the cybersecurity community.
-Hosted Region
