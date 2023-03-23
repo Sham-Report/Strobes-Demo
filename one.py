252bdd9e422fc157d8d2d7bf512cf459d6cc26dc
Sham-Report <63191240+Sham-Report@users.noreply.github.com>
2023-03-23 13:29:16 +0530

    Update one.py

--- one.py
+++ one.py
@@ -20,3 +20,13 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
+    
+
+app.run(debug=True)
+app.run(debug=True)
+app.run(debug=True)
+app.run(debug=True)
+app.run(debug=True)
+    
